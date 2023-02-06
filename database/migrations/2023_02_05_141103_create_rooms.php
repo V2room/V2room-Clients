373eb0c187f1b166d5b3ad4ab0bfb9bbd04985ef
Wilson <zerad3208@gmail.com>
2023-02-06 22:53:17 +0900

    Update 2023_02_05_141103_create_rooms.php

--- database/migrations/2023_02_05_141103_create_rooms.php
+++ database/migrations/2023_02_05_141103_create_rooms.php
@@ -1,31 +1,14 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends \LaravelSupports\Libraries\Supports\Databases\Migrations\CreateMigration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('rooms', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    protected string $table = 'rooms';
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    protected function defaultUpTemplate(Blueprint $table)
     {
-        Schema::dropIfExists('rooms');
+        $table->id();
+        $table->timestamps();
     }
 };
