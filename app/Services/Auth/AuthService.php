bf2c8d91432999f4d09a3f927ad695e7e07e6a1f
Wilson <zerad3208@gmail.com>
2023-02-17 19:30:43 +0900

    Update AuthService.php

--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -3,8 +3,12 @@
 namespace App\Services\Auth;
 
 use App\Services\BaseService;
+use Illuminate\Support\Facades\Auth;
 
 class AuthService extends BaseService
 {
-
+    public function getUser(): \Illuminate\Contracts\Auth\Authenticatable
+    {
+        return Auth::user();
+    }
 }
