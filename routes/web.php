980b6b353e0d4642cd5170ba5a19441b269d2377
Wilson <zerad3208@gmail.com>
2023-02-21 16:56:49 +0900

    add livewire package

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/list', function () {
+    return view('list');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
