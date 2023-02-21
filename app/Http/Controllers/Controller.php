a5948eca1726383d255943a7791e3723973a97c0
Wilson <zerad3208@gmail.com>
2023-02-21 20:43:57 +0900

    Update Controller.php
    add locale

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Facades\App;
 
 class Controller extends \LaravelSupports\Controllers\BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function init()
+    {
+        $locale = request('locale', config('app.locale'));
+        App::setLocale($locale);
+    }
+
+
 }
