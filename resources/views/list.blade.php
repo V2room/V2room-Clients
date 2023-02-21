883d7068d8e6884132ad5dc854979fd996675433
Wilson <zerad3208@gmail.com>
2023-02-21 19:24:21 +0900

    add livewire
    add list blade

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -1,127 +1,23 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-
-        <ul class="list-style-none">
-            <li class="d-flex no-block card-body">
-                <i class="fa fa-check-circle w-30px m-t-5"></i>
-                <div>
-                    <a href="#" class="m-b-0 font-medium p-0" data-abc="true">Version 2.5 released</a>
-                    <span class="text-muted">A new version 2.5 has been released. </span>
-                </div>
-                <div class="ml-auto">
-                    <div class="tetx-right">
-                        <h5 class="text-muted m-b-0">26</h5>
-                        <span class="text-muted font-16">FEB</span>
+    <div class="py-8">
+        @for ($i = 0; $i < 10; $i++)
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 rounded-xl shadow-md overflow-hidden py-2">
+                <div class="md:flex bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="md:shrink-0">
+                        <img class="h-48 w-full object-cover md:h-full md:w-20" src="/resources/images/blank.webp"
+                             alt="">
+                    </div>
+                    <div class="p-4">
+                        <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                            Title
+                        </div>
+                        <a href="#"
+                           class="block mt-1 text-sm leading-tight font-medium hover:underline text-gray-900 dark:text-gray-100">
+                            Tags
+                        </a>
                     </div>
                 </div>
-            </li>
-    </div>
-    <div class="row d-flex justify-content-center mt-100 mb-100">
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-body text-center">
-                    <h4 class="card-title m-b-0">News Updates</h4>
-                </div>
-                <ul class="list-style-none">
-                    <li class="d-flex no-block card-body">
-                        <i class="fa fa-check-circle w-30px m-t-5"></i>
-                        <div>
-                            <a href="#" class="m-b-0 font-medium p-0" data-abc="true">Version 2.5 released</a>
-                            <span class="text-muted">A new version 2.5 has been released. </span>
-                        </div>
-                        <div class="ml-auto">
-                            <div class="tetx-right">
-                                <h5 class="text-muted m-b-0">26</h5>
-                                <span class="text-muted font-16">FEB</span>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="d-flex no-block card-body border-top">
-                        <i class="fa fa-gift w-30px m-t-5"></i>
-                        <div>
-                            <a href="#" class="m-b-0 font-medium p-0" data-abc="true">Congratulation to AAA For new investment</a>
-                            <span class="text-muted">AAA has invested $2M in MMM. we are happy to working forward with AAA.</span>
-                        </div>
-                        <div class="ml-auto">
-                            <div class="tetx-right">
-                                <h5 class="text-muted m-b-0">11</h5>
-                                <span class="text-muted font-16">MAR</span>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="d-flex no-block card-body border-top">
-                        <i class="fa fa-plus w-30px m-t-5"></i>
-                        <div>
-                            <a href="#" class="m-b-0 font-medium p-0" data-abc="true">Hiring Android Developers</a>
-                            <span class="text-muted">We are hiring android developers to expend our mobile development team.</span>
-                        </div>
-                        <div class="ml-auto">
-                            <div class="tetx-right">
-                                <h5 class="text-muted m-b-0">19</h5>
-                                <span class="text-muted font-16">APR</span>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="d-flex no-block card-body border-top">
-                        <i class="fa fa-leaf w-30px m-t-5"></i>
-                        <div>
-                            <a href="#" class="m-b-0 font-medium p-0" data-abc="true">Anounced New CEO</a>
-                            <span class="text-muted">We announced John kendey as new CEO of the company.</span>
-                        </div>
-                        <div class="ml-auto">
-                            <div class="tetx-right">
-                                <h5 class="text-muted m-b-0">20</h5>
-                                <span class="text-muted font-16">JUN</span>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="d-flex no-block card-body border-top">
-                        <i class="fa fa-question-circle w-30px m-t-5"></i>
-                        <div>
-                            <a href="#" class="m-b-0 font-medium p-0" data-abc="true">New Guidelines to Employees</a>
-                            <span class="text-muted">We have realeased new guidelines to all employees</span>
-                        </div>
-                        <div class="ml-auto">
-                            <div class="tetx-right">
-                                <h5 class="text-muted m-b-0">15</h5>
-                                <span class="text-muted font-16">AUG</span>
-                            </div>
-                        </div>
-                    </li>
-                    <li class="d-flex no-block card-body border-top">
-                        <i class="fa fa-plus w-30px m-t-5"></i>
-                        <div>
-                            <a href="#" class="m-b-0 font-medium p-0" data-abc="true">Android App version 5.1 released</a>
-                            <span class="text-muted">We have realeased android app version 5.1. update now.</span>
-                        </div>
-                        <div class="ml-auto">
-                            <div class="tetx-right">
-                                <h5 class="text-muted m-b-0">19</h5>
-                                <span class="text-muted font-16">OCT</span>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
             </div>
-
-        </div>
+        @endfor
     </div>
-
 </x-app-layout>
