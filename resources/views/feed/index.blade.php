56731ac5c4dda74b48f4f6b97b8acccd74ca9cf6
Wilson <zerad3208@gmail.com>
2023-02-23 20:20:30 +0900

    Update index.blade.php

--- resources/views/feed/index.blade.php
+++ resources/views/feed/index.blade.php
@@ -7,22 +7,23 @@
 
     <div class="py-8">
         @for ($i = 0; $i < 10; $i++)
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 rounded-xl shadow-md overflow-hidden py-2">
-                <div class="md:flex bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="md:shrink-0">
-                        <img class="h-48 w-full object-cover md:h-full md:w-20" src="/resources/images/blank.webp"
-                             alt="">
-                    </div>
-                    <div class="p-2">
-                        <div class="uppercase tracking-wide text-md text-indigo-500 font-semibold">
-                            Title
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 rounded-xl overflow-hidden py-2">
+                <div class="flex space-x-4 items-center bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <a href="#" class="block">
+                        <img src="https://picsum.photos/200/200" alt="Post Thumbnail" class="w-20 h-20 object-cover">
+                    </a>
+                    <div class="flex flex-col">
+                        <a href="#" class="text-lg font-medium text-gray-900 font-semibold text-indigo-50">
+                            Post Title
+                        </a>
+                        <div class="mb-1">
+                            <p class="text-sm text-gray-400 mb-1">category</p>
                         </div>
-                        <p class="block mt-1 text-sm leading-tight font-medium hover:underline text-gray-900 dark:text-gray-100">
-                            category
-                        </p>
-                        <p class="text-sm text-slate-500">
-                            tags
-                        </p>
+                        <div class="mb-1">
+                            <span class="bg-gray-200 rounded-full text-xs px-2 py-1 mr-2 mb-2">tag</span>
+                            <span class="bg-gray-200 rounded-full text-xs px-2 py-1 mr-2 mb-2">tag</span>
+                            <span class="bg-gray-200 rounded-full text-xs px-2 py-1 mr-2 mb-2">tag</span>
+                        </div>
                     </div>
                 </div>
             </div>
